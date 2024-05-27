c011114791bc179a551ccdb97f804259fe6db4cf
6luv <jmj00005@naver.com>
2024-05-27 13:50:18 +0900

    refactor: category card spacing

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,6 +1,6 @@
 const CategoryCard = ({ icon, title }) => {
   return (
-    <div className="flex flex-col items-center justify-center mx-6">
+    <div className="flex flex-col items-center justify-center max-w-[120px] w-full mx-2">
       <button className="w-[72px] h-[72px] border-2 rounded-3xl p-5 flex items-center justify-center">
         {icon}
       </button>
