dff0ccf07e5dac5546531b5336c775c6b34c7b2d
6luv <jmj00005@naver.com>
2024-05-27 17:14:25 +0900

    feat: slide banner button

--- src/components/SlideBanner.jsx
+++ src/components/SlideBanner.jsx
@@ -2,32 +2,38 @@ import Slider from "react-slick";
 import SlideBannerCard from "./SlideBannerCard";
 import { useRef } from "react";
 import SlideBannerData from "../data/slideBannerData.json";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const SlideBanner = () => {
-  // const [page, setPage] = useState(0);
+  const [page, setPage] = useState(0);
 
   const sliderRef = useRef();
 
-  // const onClickNext = () => {
-  //   sliderRef.current.slickNext();
-  //   getCurrentPage();
-  // };
+  const onClickPrev = () => {
+    sliderRef.current.slickPrev();
+    getCurrentPage();
+  };
 
-  // const onClickPrev = () => {
-  //   sliderRef.current.slickPrev();
-  //   getCurrentPage();
-  // };
+  const onClickNext = () => {
+    sliderRef.current.slickNext();
+    getCurrentPage();
+  };
 
-  // const getCurrentPage = () => {
-  //   setPage(sliderRef.current.innerSlider.state.currentSlide);
-  // };
+  const getCurrentPage = () => {
+    setPage(sliderRef.current.innerSlider.state.currentSlide);
+  };
 
-  // useEffect(() => {
-  //   console.log(sliderRef);
-  // }, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getCurrentPage();
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div>
+    <div className="relative">
       <Slider
         ref={sliderRef}
         fade={true}
@@ -47,6 +53,49 @@ const SlideBanner = () => {
           />
         ))}
       </Slider>
+      <div className="absolute bottom-[60px] left-[330px] -translate-x-1/2">
+        <div className="flex rounded-[100px] text-white bg-[#2c323b] font-normal py-1 px-2">
+          <button onClick={onClickPrev}>
+            <svg
+              data-v-5fc24e94=""
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="transition rotate-180 text-white"
+            >
+              <path
+                fill="currentColor"
+                fillRule="evenodd"
+                d="m9 16.34 4.58-4.59L9 7.16l1.41-1.41 6 6-6 6z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+          <div className="flex">
+            <div>{page + 1}</div>
+            <div className="text-gray-200 mx-1">/</div>
+            <div>3</div>
+          </div>
+          <button onClick={onClickNext}>
+            <svg
+              data-v-5fc24e94=""
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="text-white"
+            >
+              <path
+                fill="currentColor"
+                fillRule="evenodd"
+                d="m9 16.34 4.58-4.59L9 7.16l1.41-1.41 6 6-6 6z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/SlideBannerCard.jsx
+++ src/components/SlideBannerCard.jsx
@@ -7,26 +7,26 @@ const SlideBannerCard = ({
   isBlack,
 }) => {
   return (
-    <div
-      className={`h-[400px] w-full ${isBlack ? "bg-black" : "bg-[#D5C6FF]"} `}
-    >
-      <div className="flex px-5 justify-center">
-        <div
-          className={`flex flex-col justify-center ${
-            isBlack ? "text-white" : "text-black"
-          }`}
-        >
-          <h5 className="mb-2">{label}</h5>
-          <h3 className="font-semibold text-[26px]">{title1}</h3>
-          <h3 className="font-semibold text-[26px]">{title2}</h3>
-          <h5 className="mt-4">{description}</h5>
-        </div>
-        <div>
-          <img
-            className="h-[400px] object-cover w-full"
-            src={`/images/banners/${id}.png`}
-            alt={label}
-          />
+    <div className="h-[400px]">
+      <div className={`w-full ${isBlack ? "bg-black" : "bg-[#D5C6FF]"} `}>
+        <div className="flex px-5 justify-center items-center">
+          <div
+            className={`flex flex-col justify-center max-w-[350px] w-full ${
+              isBlack ? "text-white" : "text-black"
+            }`}
+          >
+            <h5 className="mb-2">{label}</h5>
+            <h3 className="font-bold text-[26px]">{title1}</h3>
+            <h3 className="font-bold text-[26px]">{title2}</h3>
+            <h5 className="mt-4">{description}</h5>
+          </div>
+          <div>
+            <img
+              className="h-[400px] object-cover w-full"
+              src={`/images/banners/${id}.png`}
+              alt={label}
+            />
+          </div>
         </div>
       </div>
     </div>
