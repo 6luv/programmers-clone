c31fb1b9f2187d6e59982272aebd82b639b17d08
6luv <jmj00005@naver.com>
2024-05-27 10:29:50 +0900

    feat: education section

--- src/components/EducationCard.jsx
+++ src/components/EducationCard.jsx
@@ -1,17 +1,17 @@
-const EducationCard = () => {
+const EducationCard = ({ image, title }) => {
   return (
-    <div className="h-[447px] w-[326px]">
-      <img src="images/educations/1.png" className="rounded-t-xl" />
-      <div className="p-6 border-[1px] border-[#dce1e5] rounded-b-xl">
+    <div className="h-auto max-w-[370px] mt-10 flex flex-col">
+      <img src={`images/educations/${image}.png`} className="rounded-t-xl" />
+      <div className="p-6 border-[1px] border-[#dce1e5] rounded-b-xl flex flex-col justify-between flex-grow">
         <div className="mb-6">
-          <h3 className="mb-4 text-programmers-main font-bold text-[18px] rounded-b-xl">
-            [1기] K-Digital Training: 클라우드 기반 백엔드 엔지니어링
+          <h3 className="mb-4 text-programmers-main font-bold text-[18px]">
+            {title}
           </h3>
-          <div className="flex">
-            <div className="py-1 px-[6px] rounded-[4px] font-medium text-xs text-[#0078ff] bg-[#ebf5ff] mr-2">
+          <div className="flex flex-wrap gap-2">
+            <div className="py-1 px-[6px] rounded-[4px] font-medium text-xs text-[#0078ff] bg-[#ebf5ff]">
               모집 중 | D-25
             </div>
-            <div className="py-1 px-[6px] rounded-[4px] font-medium text-xs text-[#ff850a] bg-[#fff7de] mr-2">
+            <div className="py-1 px-[6px] rounded-[4px] font-medium text-xs text-[#ff850a] bg-[#fff7de]">
               오픈 알림 신청
             </div>
             <div className="py-1 px-[6px] rounded-[4px] font-medium text-xs text-[#3ab03f] bg-[#ebfae8]">
@@ -19,7 +19,7 @@ const EducationCard = () => {
             </div>
           </div>
         </div>
-        <div className="pt-4 mt-[27px] text-programmers-main font-bold border-t border-[#dce1e5]">
+        <div className="pt-4 mt-auto text-programmers-main font-bold border-t border-[#dce1e5]">
           무료
         </div>
       </div>
--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -1,12 +1,38 @@
 import EducationCard from "./EducationCard";
 import TitleBar from "./TitleBar";
 
+const EducationData = [
+  {
+    id: 1,
+    image: "1",
+    title: "[1기] K-Digital Training: 클라우드 기반 백엔드 엔지니어링",
+  },
+  {
+    id: 2,
+    image: "2",
+    title: "[1기] K-Digital Training: 클라우드 기반 프론트엔드 엔지니어링",
+  },
+  {
+    id: 3,
+    image: "3",
+    title:
+      "[3기] K-Digital Training: 타입스크립트로 함께하는 웹 풀 사이클 개발(React, Node.js)",
+  },
+  {
+    id: 4,
+    image: "4",
+    title: "[3기] K-Digital Training: 데이터 분석 데브코스",
+  },
+];
+
 const EducationSection = () => {
   return (
     <main>
       <TitleBar title="국비지원으로 개발자 커리어 시작!" />
-      <div className="w-[1160px] mt-14 flex justify-between mx-auto">
-        <EducationCard />
+      <div className="w-[1160px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
+        {EducationData.map((v) => (
+          <EducationCard key={v.id} image={v.image} title={v.title} />
+        ))}
       </div>
     </main>
   );
